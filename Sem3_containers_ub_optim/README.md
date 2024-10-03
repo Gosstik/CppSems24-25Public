9402afa6155b3965972f350916e29488d52d4aaf
Egor Vashkevich <goshikvash@gmail.com>
2024-10-04 01:12:57 +0300

    Sem3

---
+++ Sem3_containers_ub_optim/README.md
@@ -0,0 +1,11 @@
+# Семинар 3
+
+## Kinds of memory
+
+- Static
+- Stack
+- Dynamic (heap)
+
+## TODO
+
+Строки
---
+++ Sem3_containers_ub_optim/godbolt_optim.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  int ind = 0;
+  for (int i = 0; i < 10; ++i) {
+    ++ind;
+  }
+  int j = 1;
+  std::cout << j << std::endl;
+}

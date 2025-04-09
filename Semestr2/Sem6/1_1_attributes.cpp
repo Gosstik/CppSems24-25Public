54c156d0fcef534561782bd9078f1123308ec252
Egor Vashkevich <goshikvash@gmail.com>
2025-04-09 08:45:10 +0300

    add Sem6

---
+++ Semestr2/Sem6/1_1_attributes.cpp
@@ -0,0 +1,11 @@
+// TODO: maybe place after allocator
+
+// https://en.cppreference.com/w/cpp/language/attributes
+
+// no_unique_address
+// maybe_unused --- variable maybe unused because of compile time condition
+// nodiscard("memory leak") --- new, allocate, empty
+// fallthrough
+// likely, unlikely
+// deprecated
+// noreturn

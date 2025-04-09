3079c3b0bf09920d0d08b928bb37335b5adcb618
Egor Vashkevich <goshikvash@gmail.com>
2025-04-09 08:44:25 +0300

    add Sem5

---
+++ Semestr2/Sem5_containers/1_1_container_adapters.cpp
@@ -0,0 +1,8 @@
+
+// clang-format off
+
+// Why adaptor accepts Container as template parameter but not as template template?
+// --- Container is able to have several template parameters and
+// even values (std::array, so variadic is also not suitable)
+
+// clang-format on
---
+++ Semestr2/Sem5_containers/4_1_bit_fields.cpp
@@ -0,0 +1,7 @@
+
+// Bit Fields
+// https://en.cppreference.com/w/cpp/language/bit_field
+
+// Convenient for parsing network packets (with reinterpret_cast).
+
+// TODO: std::bit_cast
